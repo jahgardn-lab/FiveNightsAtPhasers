81226fff9ad6cc1b81652be91565140228292917
dylanM9 <dpmcderm@ucsc.edu>
2026-05-31 16:06:01 -0700

    Implemented basic Cam System & editable cam button system. also added spacebar keybind to flip cameras

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -8,16 +8,40 @@ class Load extends Phaser.Scene {
 
         // load camera button
         this.load.image("cameraButton", "cameraButton.png");
+        // camera location buttons
+        this.load.image("roomButton", "roomsButtons.png");
+        // load office sprite
         this.load.image("office", "officePlaceholder.png");
+        
+        // load default camera image
+        this.load.image("camera_placeholder", "camera_placeholder.png");
+
+        // THE GREAT CAMERA BACKGROUND DUMP
+        this.load.image("hallway_right", "hallway_right.png");
+        this.load.image("hallway_left", "hallway_left.png");        
 
 
-        // Load tilemap information
-        //this.load.image("tilemap_tiles", "tilemap_packed.png");                   // Packed tilemap
-        //this.load.tilemapTiledJSON("three-farmhouses", "three-farmhouses.tmj");   // Tilemap in JSON
+
+
+        // load rooms json file
+        this.load.json('roomData', 'roomData.json');
     }
 
     create() {
-        
+
+        // access var that lets us read the room data
+        //const roomD = this.cache.json.get('roomData');
+
+        //console.log(roomD.rooms[0].connectedRooms[0]); // returns, "Office"
+        //console.log(roomD.rooms[1].connectedRooms[1]); // returns, "leftHallway"
+        //console.log(roomD.rooms[0]); // returns leftHallway room data
+
+
+
+
+
+
+
 
          // ...and pass to the next Scene
          this.scene.start("testNightScene");
--- src/Scenes/testNight.js
+++ src/Scenes/testNight.js
@@ -8,13 +8,50 @@ class TestNight extends Phaser.Scene {
 
     init() {
         this.HAS_WON = false;
+        this.CAM_SHIFT_AMOUNT = 200;
+        this.CAM_SHIFT_SPEED = 15;
         this.hasToggledCams = false;
+        this.hasToggledCams_space = false;
         this.camsAreOpen = false;
     }
 
     create() {
+        // access var that lets us read the room data
+        this.roomD = this.cache.json.get('roomData');
 
+        // place office background sprite
         my.sprite.office = this.add.sprite(game.config.width/2, game.config.height/2, "office").setScale(1.8);
+
+        // create current camera sprite (defaults to one of our choice)
+        my.sprite.curCam = this.add.sprite(game.config.width/2, game.config.height/2, "camera_placeholder").setVisible(false);
+
+        // place camera button at bottom of screen
+        my.sprite.camButton = this.add.sprite(960, 1000, "cameraButton").setScale(1.5, 0.5).setAlpha(0.3);
+
+        // CAMERA BUTTONS //////////////////////////////////////////////////////////////////////
+
+        // camera buttons group
+        this.camButtons = this.add.group();
+
+                // this.createCamButton(x, y, texture, scale); ref for function
+
+        // camera button for left hallway
+        let camButton_leftHallway = this.createCamButton(1200, 800, "roomButton", 0.5);
+        // listener for camera button getting clicked
+        camButton_leftHallway.on('pointerdown', function (pointer) {
+            // pointer.x and pointer.y gives coordinates of the click
+            my.sprite.curCam.setTexture("hallway_left");
+        });
+
+        // camera button for right hallway
+        let camButton_rightHallway = this.createCamButton(1300, 800, "roomButton", 0.5);
+        // listener for camera button getting clicked
+        camButton_rightHallway.on('pointerdown', function (pointer) {
+            // pointer.x and pointer.y gives coordinates of the click
+            my.sprite.curCam.setTexture("hallway_right");
+        });
+
+        // TEXT STUFF //////////////////////////////////////////////////////////////////////////
 
         // place clock text in bottom left of screen
         this.clockText = this.add.text(1880, 80, "12:00", { font: '64px Courier', fill: '#ff0000' }).setOrigin(1, 1);
@@ -35,8 +72,8 @@ class TestNight extends Phaser.Scene {
             paused: false, // do I need to freaking tell you >:(
         });
 
-        // place camera button at bottom of screen
-        my.sprite.camButton = this.add.sprite(game.config.width/2, 1000, "cameraButton").setScale(1.5, 0.5).setAlpha(0.3);
+        // space key listener
+        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update(delta, time) {
@@ -50,7 +87,9 @@ class TestNight extends Phaser.Scene {
         let elapsedMinutes = Math.floor(elapsedSeconds/60);
         // mouse pointer tracker - remember! mouse pos is in pixels
         let pointer = this.input.activePointer;
-        
+        // current scroll position of camera
+        let shiftPos = this.cameras.main.scrollX;
+
         // TIMER STUFF /////////////////////////////////////////////////////////////////////////
 
         // DEBUG TIMER
@@ -80,31 +119,69 @@ class TestNight extends Phaser.Scene {
         // if mouse is hovering over bottom center of screen and the cams haven't been toggled, toggle cams
         /* note to anyone looking at this; YES I could have used the built in sprite hovering
         from the mouse pointer, but it was being super finnicky, so hard coding it is :) */
-        if(pointer.x > 400 && pointer.x < 1520 && pointer.y > 950 && this.hasToggledCams == false) {
-
-            // toggle current cam state
-            this.camsAreOpen = !this.camsAreOpen;
+        if((pointer.x > 400 && pointer.x < 1520 && pointer.y > 950 && !this.hasToggledCams) || (this.spaceKey.isDown && !this.hasToggledCams_space)) {
 
             // if cams are open, close them
             if(this.camsAreOpen) {
 
-
+                // TODO: implement camera closing logic
+                my.sprite.curCam.visible = false;
+                // toggle current cam state
+                this.camsAreOpen = false;
             } else { // otherwise, open them
 
-
+                // TODO: implement camera opening logic
+                my.sprite.curCam.visible = true;
+                // toggle current cam state
+                this.camsAreOpen = true;
             }
 
             // flip button sprite
             my.sprite.camButton.flipY = !my.sprite.camButton.flipY;
+            // set camera toggle (prevents multi triggering)
             this.hasToggledCams = true;
-        } else if((pointer.x < 400 || pointer.x > 1520 || pointer.y < 950) && this.hasToggledCams == true) 
-        { this.hasToggledCams = false; }
+            // THIS IS BEING IMPLEMENTED IN A GROSS MANNER SPECIFICALLY FOR JADE; GOT A PROBLEM WITH IT? BRING IT UP WITH HER.
+            if(this.spaceKey.isDown) { this.hasToggledCams_space = true;}
+        // if mouse is not hovering over button, and camera toggle hasn't been reset, do so now
+        } else if(((pointer.x < 400 || pointer.x > 1520 || pointer.y < 950) && this.hasToggledCams) || (!this.spaceKey.isDown && this.hasToggledCams_space))
+        { this.hasToggledCams = false; if(!this.spaceKey.isDown) { this.hasToggledCams_space = false; } }
 
+        // VIEW SHIFT //////////////////////////////////////////////////////////////////////////
 
+        let burger = 0; // burger is a helper var that stops the UI from shifting all weird on the screen
+        if(pointer.x < 200 && shiftPos > -this.CAM_SHIFT_AMOUNT && !this.camsAreOpen) {
+            this.cameras.main.scrollX -= this.CAM_SHIFT_SPEED; burger = -1;
+        }
+        if(pointer.x > 1720 && shiftPos < this.CAM_SHIFT_AMOUNT && !this.camsAreOpen) {
+            this.cameras.main.scrollX += this.CAM_SHIFT_SPEED; burger = 1;
+        }
 
+        // UI STUFF ////////////////////////////////////////////////////////////////////////////
 
+        // offset ui to be relative to current screen scroll position
+        this.clockText.setPosition(1880 + shiftPos + (this.CAM_SHIFT_SPEED * burger), this.clockText.y);
+        this.nightLabel.setPosition(1880 + shiftPos + (this.CAM_SHIFT_SPEED * burger), this.nightLabel.y);
+        my.sprite.curCam.setPosition(960 + shiftPos + (this.CAM_SHIFT_SPEED * burger), 540);
+        my.sprite.camButton.setPosition(960 + shiftPos + (this.CAM_SHIFT_SPEED * burger), 1000);
 
+        // CAMERA BUTTONS //////////////////////////////////////////////////////////////////////
 
+        // loop through every camera button
+        /* hasGorbed helps prevent rerunning this code
+        over and over again, hopefully helping with performance :) */
+        this.camButtons.children.iterate((camButton) => { if(camButton) {
+
+            if(this.camsAreOpen && camButton.hasGorbed == false) {
+                // offset button location with current shift position
+                camButton.x = camButton.baseX + shiftPos;
+                camButton.setVisible(true); // set button to visible
+                camButton.hasGorbed = true;
+            }
+            else if(!this.camsAreOpen && camButton.hasGorbed == true) {
+                camButton.setVisible(false); // set button to invisible
+                camButton.hasGorbed = false;
+            }
+        }});
     }
 
     // HELPER FUNCTIONS //
@@ -112,4 +189,14 @@ class TestNight extends Phaser.Scene {
     // called when 6 AM is reached
     // can prob just use this function to load the win scene
     hasWon() { this.HAS_WON = true; }
+
+    // creates new button sprite and adds it to buttons
+    createCamButton(x, y, sprite, scale) {
+        let newGroupMember = this.camButtons.create(x, y, sprite).setInteractive();
+        newGroupMember.baseX = x; // default x position before moving
+        newGroupMember.hasGorbed = false; // helper var
+        newGroupMember.setScale(scale); // set scale of sprite
+        newGroupMember.setVisible(false);
+        return newGroupMember; // return new button
+    }
 }
